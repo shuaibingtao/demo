3c20058ef52f04efc078a3dfd7ea82555bc2a9f7
shuaibingtao <43928073+shuaibingtao@users.noreply.github.com>
2021-07-27 20:28:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
 	char *str1="ncjenvrienve";
 	printf("%s\n",str1);
 	printf("hello world!");
+	printf("helle world! main()")
 	cout << str1 << endl;
 	cout << "hello world!" << endl;
 	return 0;
